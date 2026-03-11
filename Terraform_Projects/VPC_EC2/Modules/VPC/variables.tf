f1ebe94e08491f309ea0de8da7151058f3a20855
MMOKHTA <MehrdadMokhtari1370@yahoo.com>
2026-03-11 16:07:53 +0000

    create folder structre base on three-layer and create VPC module

---
+++ Terraform_Projects/VPC_EC2/Modules/VPC/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr" {
+  description = "create a cidr_block variable"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "create a public subnet cider_block"
+  type        = string
+}
