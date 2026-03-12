fd3e82953f9355064361666ec40a2e24ed255e49
MMOKHTA <MehrdadMokhtari1370@yahoo.com>
2026-03-12 14:50:20 +0000

    add variables.tfvars into the dev environment

---
+++ Terraform_Projects/VPC_EC2/Modules/environment/dev/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_cidr" {
+  type = string
+}
+
+variable "available_zone" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
