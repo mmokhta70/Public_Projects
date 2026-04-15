85d04432a89083a35620d4ae482a920594b7e4a3
MMOKHTA <MehrdadMokhtari1370@yahoo.com>
2026-04-15 12:45:49 +0100

    create variable.tf for tag module

---
+++ Terraform_Projects/RDS_Modules_Project/modules/tags/variables.tf
@@ -0,0 +1,23 @@
+variable "environment" {
+  type = string
+  validation {
+    condition     = contains(["dev", "prod", "staging"], var.environment)
+    error_message = "environment must be dev, staging, or prod"
+  }
+}
+
+variable "owner" {
+     type = string
+     description = "Oswner of the resources"
+}
+
+variable "cost_center" {
+  type        = string
+  description = "Cost center for billing"
+}
+
+variable "extra_tags" {
+  type        = map(string)
+  default     = {}
+  description = "we can add more tags here"
+}
