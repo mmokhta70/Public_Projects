818841ede187486da747e092ef641b2a986a5301
Mehrdad261 <Mehrdad.mokhtari1370@gmail.com>
2026-04-16 05:37:27 +0100

    correct the condition for environment and added var.environment

--- Terraform_Projects/RDS-Module-Project/environments/dev/main.tf
+++ Terraform_Projects/RDS-Module-Project/environments/dev/main.tf
@@ -1,6 +1,6 @@
 
 module "tags" {
-  source       = "../../tags"
+  source       = "../../modules/tags"
   cost_centre  = var.cost_centre
   environment  = var.environment
   owner        = var.owner
