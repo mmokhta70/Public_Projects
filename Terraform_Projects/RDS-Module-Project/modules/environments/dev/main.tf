046dd5ece1caa3943e52943ab71e2bf46bde81b0
Mehrdad261 <Mehrdad.mokhtari1370@gmail.com>
2026-04-16 05:28:13 +0100

    coding dev-main

--- Terraform_Projects/RDS-Module-Project/modules/environments/dev/main.tf
+++ Terraform_Projects/RDS-Module-Project/modules/environments/dev/main.tf
@@ -1,1 +1,9 @@
 
+module "tags" {
+  source       = "../../tags"
+  cost_centre  = var.cost_centre
+  environment  = var.environment
+  owner        = var.owner
+  project_name = var.project_name
+
+}
--- Terraform_Projects/RDS-Module-Project/modules/environments/dev/variables.tf
+++ Terraform_Projects/RDS-Module-Project/modules/environments/dev/variables.tf
@@ -1,3 +1,19 @@
 variable "cidr_block" {
   type = string
 }
+
+variable "cost_centre" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "owner" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
