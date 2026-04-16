4520f02b7190b163b6f4a775006f8b571083c3ea
Mehrdad261 <Mehrdad.mokhtari1370@gmail.com>
2026-04-16 19:49:43 +0100

    change main.tf in dev

--- Terraform_Projects/RDS-Module-Project/environments/dev/main.tf
+++ Terraform_Projects/RDS-Module-Project/environments/dev/main.tf
@@ -1,9 +1,15 @@
 
 module "tags" {
   source       = "../../modules/tags"
-  cost_centre  = var.cost_centre
-  environment  = var.environment
-  owner        = var.owner
-  project_name = var.project_name
+  cost_centre  = "123-as"
+  environment  = "dev"
+  owner        = "dev-team"
+  project_name = "RDS-Module"
+}
 
+
+module "vpc" {
+  source     = "../../modules/vpc"
+  cidr_block = var.cidr_block
+  tags       = module.tags.common_tags
 }
--- Terraform_Projects/RDS-Module-Project/environments/dev/variables.tf
+++ Terraform_Projects/RDS-Module-Project/environments/dev/variables.tf
@@ -1,19 +1,4 @@
+
 variable "cidr_block" {
-  type = string
-}
-
-variable "cost_centre" {
-  type = string
-}
-
-variable "environment" {
-  type = string
-}
-
-variable "owner" {
-  type = string
-}
-
-variable "project_name" {
   type = string
 }
