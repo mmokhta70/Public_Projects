0ea7a8dc99966152a869106451c932ecfd5dbd91
Mehrdad261 <Mehrdad.mokhtari1370@gmail.com>
2026-04-17 06:36:45 +0100

    correct cidrsubnet() in networking module

--- Terraform_Projects/RDS-Module-Project/environments/dev/main.tf
+++ Terraform_Projects/RDS-Module-Project/environments/dev/main.tf
@@ -3,7 +3,7 @@ data "aws_availability_zones" "available" {
 
 }
 locals {
-  azs = slice(data.aws_availability_zones.available.name, 0, 2)
+  azs = slice(data.aws_availability_zones.available.names, 0, 2)
 }
 
 
