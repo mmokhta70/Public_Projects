4c2f1ade112537fe913323c771db56ceacd4570a
Mehrdad261 <Mehrdad.mokhtari1370@gmail.com>
2026-04-17 06:31:27 +0100

    added networking module to the dev

--- Terraform_Projects/RDS-Module-Project/environments/dev/main.tf
+++ Terraform_Projects/RDS-Module-Project/environments/dev/main.tf
@@ -1,4 +1,13 @@
 
+data "aws_availability_zones" "available" {
+
+}
+locals {
+  azs = slice(data.aws_availability_zones.available.name, 0, 2)
+}
+
+
+#------------------- tags module - Environmetn level -------------------#
 module "tags" {
   source       = "../../modules/tags"
   cost_centre  = "123-as"
@@ -7,9 +16,19 @@ module "tags" {
   project_name = "RDS-Module"
 }
 
-
+#------------------- VPC module - Environmetn level -------------------#
 module "vpc" {
   source     = "../../modules/vpc"
   cidr_block = var.cidr_block
   tags       = module.tags.common_tags
+}
+
+
+#------------------- Networking module - Environmetn level -------------------#
+module "networking" {
+  source   = "../../modules/networking"
+  vpc_id   = module.vpc.vpc_id
+  vpc_cidr = module.vpc.vpc_cidr
+  azs      = local.azs
+  tags     = module.tags.common_tags
 }
