d3771dcbc54857ae05ebaa07f5d9a27a8a89dfc4
Mehrdad261 <Mehrdad.mokhtari1370@gmail.com>
2026-04-18 12:42:54 +0100

    added ingress port variable as list of object

--- Terraform_Projects/RDS-Module-Project/modules/security/variables.tf
+++ Terraform_Projects/RDS-Module-Project/modules/security/variables.tf
@@ -14,3 +14,15 @@ variable "vpc_id" {
 variable "environment" {
   type = string
 }
+
+#==================== define ingress ports variable
+variable "ingress_config" {
+  type = list(object[{
+    from_port       = number
+    to_port         = number
+    cidr_block      = optional(list(string), [])
+    security_groups = optinal(list(string), [])
+    desciption      = string
+  }])
+  default = []
+}
