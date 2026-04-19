20fec16df699520c31127f0d6d1590f2799f4b44
Mehrdad261 <Mehrdad.mokhtari1370@gmail.com>
2026-04-19 11:22:27 +0100

    added output into the rds module

--- Terraform_Projects/RDS-Module-Project/modules/rds/outputs.tf
+++ Terraform_Projects/RDS-Module-Project/modules/rds/outputs.tf
@@ -1,1 +1,12 @@
 
+output "rds_endpoint" {
+  value = aws_db_instance.this.endpoint
+}
+
+output "rds_arn" {
+  value = aws_db_instance.this.arn
+}
+
+output "secret_arn" {
+  value = aws_secretsmanager_secret.db.arn
+}
