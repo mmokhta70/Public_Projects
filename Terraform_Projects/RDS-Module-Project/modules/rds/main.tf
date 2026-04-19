202df9d7e722eeac7a9865db33efd89637485e19
Mehrdad261 <Mehrdad.mokhtari1370@gmail.com>
2026-04-19 11:13:13 +0100

    add secret manager

--- Terraform_Projects/RDS-Module-Project/modules/rds/main.tf
+++ Terraform_Projects/RDS-Module-Project/modules/rds/main.tf
@@ -20,5 +20,12 @@ resource "aws_db_instance" "this" {
   password = var.credential.password
 
   #---------- security group config ----------#
-  db_subnet_group_name = aws_db_sunbet_group.rds.name
+  db_subnet_group_name   = aws_db_sunbet_group.rds.name
+  vpc_security_group_ids = var.security_group_ids
+}
+
+
+#=================== Secret Manager config ===================#
+resource "aws_secretsmanager_secret" "db" {
+  name = "${var.identifier}-db-secret"
 }
--- Terraform_Projects/RDS-Module-Project/modules/rds/variables.tf
+++ Terraform_Projects/RDS-Module-Project/modules/rds/variables.tf
@@ -1,6 +1,15 @@
 
+
+locals {
+  name = "${var.project_name}-${var.environment}"
+}
+
 variable "identifier" {
   type = string
+  validation {
+    condition     = length(var.identifier) > 4
+    error_message = "identifier must be larger than 4 characters"
+  }
 }
 variable "engine_version" {
   type    = number
@@ -20,10 +29,37 @@ variable "db_name" {
   type = string
 }
 
+#============ credential config ============#
+
 variable "credential" {
   type = list(object({
     username = string
     password = string
   }))
   sensitive = true
+}
+
+#============ network config ============#
+#
+#------------ sunbnets
+variable "private_subnet_ids" {
+  type = list(string)
+}
+#------------ security_group_ids
+variable "security_group_ids" {
+  type        = list(string)
+  description = "RDS security group ids"
+}
+#
+#============ global tagging ============#
+variable "tags" {
+  type = map(string)
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
 }
