2be957f45114ae0d0b9989c6629559e334ea69e0
mmokhta70 <mehrdad.mokhtari1370@gmail.com>
2026-05-11 16:05:22 +0100

    Delete Terraform_Projects/RDS-Module-Project directory
    Delete the directory because I created two files

--- Terraform_Projects/RDS-Module-Project/modules/networking/outputs.tf
+++
@@ -1,9 +0,0 @@
-#--------- Public subnets id ---------#
-output "public_subnet" {
-  value = [for i in aws_subnet.public : i.id]
-}
-
-#--------- Private subnets id ---------#
-output "private_subnet" {
-  value = [for i in aws_subnet.private : i.id]
-}
--- Terraform_Projects/RDS-Module-Project/modules/vpc/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "cidr_block" {
-  type = string
-}
-
-variable "tags" {
-  type        = map(string)
-  description = "vpc tagging here"
-}
