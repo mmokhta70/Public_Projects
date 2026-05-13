6c0132d27e2a93362f16fb1992d59399e3c1fa9c
MMOKHTA <MehrdadMokhtari1370@yahoo.com>
2026-05-13 16:49:21 +0100

    add egress block for SG-public

--- Terraform_Projects/AWS_ALB/modules/security_group/main.tf
+++ Terraform_Projects/AWS_ALB/modules/security_group/main.tf
@@ -47,4 +47,10 @@ resource "aws_security_group" "sg_public" {
     protocol    = "tcp"
     cidr_blocks = [aws_security_group.sg-alb.id]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
