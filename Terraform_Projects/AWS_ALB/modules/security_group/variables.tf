7effcd4348d9e530a1a64cf8de478ede74f691c1
MMOKHTA <MehrdadMokhtari1370@yahoo.com>
2026-05-13 16:59:29 +0100

    added variables for security group

--- Terraform_Projects/AWS_ALB/modules/security_group/variables.tf
+++ Terraform_Projects/AWS_ALB/modules/security_group/variables.tf
@@ -1,9 +1,21 @@
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC_ID"
+}
 variable "project_name" {
   type        = string
   description = "value"
 }
 
-variable "tags" {
+variable "environment" {
+  type        = string
+  description = "environment name (dev, prod, stagging)"
+}
+
+
+variable "common_tags" {
   type        = map(string)
+  default     = {}
   description = "tags comes from commn_tags for resources"
 }
