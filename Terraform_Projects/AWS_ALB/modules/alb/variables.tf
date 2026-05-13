34fb99226cfadf4650d64804d677a9ffb6282fa3
Mehrdad261 <Mehrdad.mokhtari1370@gmail.com>
2026-05-13 21:41:56 +0100

    add vpc_id, public_subnet_ids, alb_sg_id, common_tags , certificate_arn inot varibales

--- Terraform_Projects/AWS_ALB/modules/alb/variables.tf
+++ Terraform_Projects/AWS_ALB/modules/alb/variables.tf
@@ -4,5 +4,34 @@ variable "project_name" {
 }
 
 variable "environment" {
+  type        = string
+  description = "environment includes prod, dev, stagging"
+}
 
+variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  type        = list(string)
+  description = "public subnet ids for alb"
+}
+
+
+variable "alb_sg_id" {
+  type        = string
+  description = "alb seurity group id that comes form security group module"
+}
+
+variable "common_tags" {
+  type    = map(string)
+  default = {}
+}
+
+
+
+variable "certificate_arn" {
+  type        = string
+  description = "ACM certificate ARN for HTTPs, if is empty => no https listener"
+  default     = ""
 }
