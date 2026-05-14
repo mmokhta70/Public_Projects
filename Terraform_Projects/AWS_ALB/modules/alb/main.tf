1711dfb1e4829bdcc50b2a90e648e41bedc6b3b9
MMOKHTA <MehrdadMokhtari1370@yahoo.com>
2026-05-14 12:16:15 +0100

    add lsitener for port 443(HTTPS)

--- Terraform_Projects/AWS_ALB/modules/alb/main.tf
+++ Terraform_Projects/AWS_ALB/modules/alb/main.tf
@@ -60,3 +60,24 @@ tags = merge(var.common_tags , {
   Name = "${var.project_name}-${var.environment}-alb-listener"
 })
 }
+
+#===================================
+# Create LISTENER -  Port 443
+#===================================
+resource "aws_alb_listener" "main_alb_listener_HTTPS"{
+  count = var.certificate_arn != "" ? 1 : 0 #only created when certificate_arn is provided
+  load_balancer_arn = aws_alb.main_alb.arn
+  port = 443
+  protocol = "HTTPS"
+  certificate_arn = var.certificate_arn
+
+  default_action {
+    type= "forward"
+    target_group_arn = aws_alb_target_group.main_alb_tg.arn
+  }
+
+  tags= merge(var.common_tags , {
+    Name = "${var.project_name}-${var.environment}-alb-listener-HTTPS"
+  })
+
+}
