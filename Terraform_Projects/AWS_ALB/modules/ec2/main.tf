ad3717bc3f99b30fc253fa01036e9d52067ec7aa
MMOKHTA <MehrdadMokhtari1370@yahoo.com>
2026-05-14 14:50:29 +0100

    create key-pair for ssh to the security group

--- Terraform_Projects/AWS_ALB/modules/ec2/main.tf
+++ Terraform_Projects/AWS_ALB/modules/ec2/main.tf
@@ -12,3 +12,14 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
+
+#The key pair is only useful if the SG lets SSH through.
+resource "aws_key_pair" "this" {
+  key_name = "${var.project_name}-${var.environment}-key"
+  public_key = file("~/.ssh/${var.project_name}-${var.environment}-key.pub")
+  tags = merge(var.common_tags, {
+    "Name" = "${var.project_name}-${var.environment}-key"
+  })
+}
+
+
