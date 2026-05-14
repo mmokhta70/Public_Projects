6220133a70c75e7aba9bfe8684f45690cd9f8c42
MMOKHTA <MehrdadMokhtari1370@yahoo.com>
2026-05-14 12:48:45 +0100

    added validation for instance_type

--- Terraform_Projects/AWS_ALB/modules/ec2/main.tf
+++ Terraform_Projects/AWS_ALB/modules/ec2/main.tf
@@ -1,6 +1,8 @@
 variable "instance_type" {
 type = string
 description = "The type of instance like t2.micro, t3.micro, etc."
+validation {
+  condition = contains (["t3.micro", "t3.medium", "t3.large"], var.instance_type)
+  error_message = "Please make sure you choose the correct instance type. Allowed values are: t3.micro, t3.medium, t3.large."
+  }
 }
-
-
