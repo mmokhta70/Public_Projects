b1dbdb136374de97eee469bed9ce743e549b2384
MMOKHTA <MehrdadMokhtari1370@yahoo.com>
2026-05-14 16:49:48 +0100

    add route table association for private subnets

--- Terraform_Projects/AWS_ALB/modules/Networking/main.tf
+++ Terraform_Projects/AWS_ALB/modules/Networking/main.tf
@@ -100,3 +100,13 @@ resource "aws_route_table_association" "public_rtb_assocication" {
      subnet_id = aws_subnet.public_subnet[count.index].id
      route_table_id = aws_route_table.public_rtb.id
 }
+
+
+#====================================
+# Create route table association for private subnet
+#====================================
+resource "aws_route_table_association" "private_rtb_association" {
+     count = length(local.azs)
+     subnet_id = aws_subnet.private_subnet[count.index].id
+     route_table_id = aws_route_table.private_rtb.id
+}
