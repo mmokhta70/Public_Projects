4452115bfcf686045a4025d3462e61f5bca23a94
MMOKHTA <MehrdadMokhtari1370@yahoo.com>
2026-05-14 15:58:35 +0100

    add networking module and create mains file

---
+++ Terraform_Projects/AWS_ALB/modules/Networking/main.tf
@@ -0,0 +1,10 @@
+#=====================================
+# fetch all the az in your region 
+#=====================================
+locals {
+     azs = data.aws_availability_zone.available.names
+}
+
+data "aws_availability_zone" "available" {
+state = "available"
+}
