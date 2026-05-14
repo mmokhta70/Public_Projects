236ceb2cb4a77ab65121a391ffbb2c0c88f3fef3
MMOKHTA <MehrdadMokhtari1370@yahoo.com>
2026-05-14 16:11:44 +0100

    add common_tags and project_name , environment variables

--- Terraform_Projects/AWS_ALB/modules/Networking/main.tf
+++ Terraform_Projects/AWS_ALB/modules/Networking/main.tf
@@ -25,4 +25,7 @@ locals {
 #====================================
 resource "aws_internet_gateway" "igw" {
      vpc_id = var.vpc_id
+     tags = merge (var.common_tags , {
+          Name = "£{var.project_name}-${var.environment}-igw"
+     })
 }
--- Terraform_Projects/AWS_ALB/modules/Networking/variables.tf
+++ Terraform_Projects/AWS_ALB/modules/Networking/variables.tf
@@ -7,3 +7,19 @@ variable "vpc_id" {
      type = string
      description = "vpc id"
 }
+
+variable "project_name" {
+     type = string
+     description = "project name"
+}
+
+variable "environment" {
+     type = string
+     description = "environment name"
+}    
+
+variable "common_tags" {
+     type = map(string)
+     description = "common tags for all resources"
+     default = {}
+}
