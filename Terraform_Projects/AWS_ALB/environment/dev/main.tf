53c9075704bc243e20ecdef1b5625f02c1af6bf9
MMOKHTA <MehrdadMokhtari1370@yahoo.com>
2026-05-15 10:07:07 +0100

    add security_group module into the dev environment

--- Terraform_Projects/AWS_ALB/environment/dev/main.tf
+++ Terraform_Projects/AWS_ALB/environment/dev/main.tf
@@ -53,3 +53,18 @@ module "networking" {
   environment  = var.environment
   project_name = var.project_name
 }
+
+
+
+
+
+
+#==================================
+# security group module
+#==================================
+module "security_group" {
+  source       = "../../modules/security_group"
+  vpc_id       = module.vpc.vpc_id
+  environment  = var.environment
+  project_name = var.project_name
+}
